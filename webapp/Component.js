50fbfca6fd999d5451e82030b957d49eadd7c71f
Manjunath NJ <manjunath.j11@wipro.com>
2020-04-09 08:14:29 +0000

    Initial staging

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/ui/generic/app/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("com.practice.ZFioriElementsAnalyticalList.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
